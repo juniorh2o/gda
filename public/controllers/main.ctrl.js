d6bbeb36c7f7191d7fb6c97564400c4f2f6cae6c
Sidney <sidney.schneider@vejoaovivo.com.br>
2016-11-13 12:38:33 -0200

    added menu from database data

--- public/controllers/main.ctrl.js
+++ public/controllers/main.ctrl.js
@@ -2,6 +2,15 @@ angular.module('gdaApp').controller('MainController',
     ['$scope', 'graphList',
         function ($scope, graphList) {
             $scope.graphList = graphList.data;
+
+            $scope.selectedMenu = 'discipline';
+            $scope.selectedOption = 0;
+
+            $scope.changeSelectedOption = function (menu, option) {
+                $scope.selectedMenu = menu;
+                $scope.selectedOption = option;
+            }
+
         }
     ]
 );
